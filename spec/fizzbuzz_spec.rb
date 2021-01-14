1232bb7bc67836083cc6fc005562e436993441ab
Adam Jonas <adamjonas25@gmail.com>
2021-01-14 12:07:44 +0000

    refactor

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,15 +13,19 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq('Buzz')
   end
 
-  it "returns Fizz when passed 6" do
+  it "returns Fizz when passed multiples of 3" do
     [3,6,9,12,18].each { |n| expect(fizzbuzz(n)).to eq 'Fizz' }
+  end
+
+  it "returns Buzz when passed multiples of 5" do
+    [5,10,20].each { |n| expect(fizzbuzz(n)).to eq 'Buzz' }
   end
 
   it "returns FizzBuzz when passed 15" do
     expect(fizzbuzz(15)).to eq 'FizzBuzz'
   end
 
-  it 'returns n if not divisible by 3 or 5' do
+  it "returns number if number is not divisible by 3 or 5" do
     [1,2,4,7,8,11,13,14,16,17,19].each { |n| expect(fizzbuzz(n)).to eq n }
   end
 end
