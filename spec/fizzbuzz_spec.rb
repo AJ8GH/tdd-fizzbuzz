ce6921a85f682f952c58ef81ba5ff887b47c690f
Adam Jonas <adamjonas25@gmail.com>
2021-01-14 11:37:39 +0000

    pass all specs

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -16,5 +16,12 @@ describe 'fizzbuzz' do
   it "returns Fizz when passed 6" do
     [3,6,9,12,18].each { |n| expect(fizzbuzz(n)).to eq 'Fizz' }
   end
-  
+
+  it "returns FizzBuzz when passed 15" do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+
+  it 'returns n if not divisible by 3 or 5' do
+    [1,2,4,7,8,11,13,14,16,17,19].each { |n| expect(fizzbuzz(n)).to eq n }
+  end
 end
