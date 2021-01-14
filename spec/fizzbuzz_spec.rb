053f44b4ec9bb9b31077cb9028047164c1b6317d
Adam Jonas <adamjonas25@gmail.com>
2021-01-14 10:59:28 +0000

    pass 3 specs

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/fizzbuzz'
+
+describe 'fizzbuzz' do
+  it "returns Buzz when passed 3" do
+    expect(fizzbuzz(3)).to eq('Fizz')
+  end
+
+  it "returns Buzz when passed 5" do
+    expect(fizzbuzz(5)).to eq('Buzz')
+  end
+end
